12a459e823c0af87a384f6ba9f7e4f4123e907ba
adriancgarcia <adriancgarcia@gmail.com>
2023-06-18 20:20:15 -0500

    new page created.

--- server.js
+++ server.js
@@ -9,19 +9,26 @@ app.get('/pokemon', (req, res) => {
     res.render('index.ejs', {pokemon});
     });
     
+// NEW - GET 
+app.get('/pokemon/new', (req, res) => {
+    res.render('pokemon/new.ejs')
+})
+
+
+// CREATE - POST 
+// get.post('/', (req, res) => {
+// pokemon.create(req.body).then(()=> {
+//     res.redirect('/pokemon')
+//     })
+// })
+
     
 // SHOW
 app.get('/pokemon/:id', (req, res) => {
     const id = req.params.id;
     const Pokemon = pokemon[id];
-    
 res.render('show.ejs', {Pokemon, id});
-
 });
-
-
-
-
 
 app.listen(3000, () => {
     console.log("Listening");
